339eaf9fc971be270b9bb5fa08cb956967a41b8d
brisalchert <brisalchert@gmail.com>
2024-12-04 17:01:27 -0500

    Add tech stack icons to App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from "./components/Header.jsx";
+import TechStack from "./components/TechStack.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Header />
+      <TechStack />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/components/TechStack.jsx
@@ -0,0 +1,17 @@
+import StackIcon from "tech-stack-icons";
+import "./TechStack.css"
+
+export default function TechStack() {
+  return (
+    <div className="tech-stack">
+      <StackIcon className="tech-stack__icon" name="python" />
+      <StackIcon className="tech-stack__icon" name="java" />
+      <StackIcon className="tech-stack__icon" name="postgresql" />
+      <StackIcon className="tech-stack__icon" name="git" />
+      <StackIcon className="tech-stack__icon" name="html5" />
+      <StackIcon className="tech-stack__icon" name="css3" />
+      <StackIcon className="tech-stack__icon" name="js" />
+      <StackIcon className="tech-stack__icon" name="reactjs" />
+    </div>
+  )
+}
