3319e1ab1cfacece778204f300b4317b0a2b74a8
brisalchert <brisalchert@gmail.com>
2024-12-04 17:42:20 -0500

    Remove template logos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 import Header from "./components/Header.jsx";
 import TechStack from "./components/TechStack.jsx";
 
@@ -12,14 +10,6 @@ function App() {
     <>
       <Header />
       <TechStack />
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
