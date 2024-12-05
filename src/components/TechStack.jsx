f30184e1519fb0fd9ee5f1e63341a9230a273844
Brian Salchert <brisalchert@gmail.com>
2024-12-04 23:13:47 -0500

    Add hover text for tech stack icons

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,19 +1,50 @@
+import PropTypes from 'prop-types';
 import StackIcon from "tech-stack-icons";
-import "./TechStack.css"
+import "./TechStack.css";
 
 export default function TechStack() {
-  return (
-    <div className="tech-stack">
-      <StackIcon className="tech-stack__icon" name="python" />
-      <StackIcon className="tech-stack__icon" name="java" />
-      <StackIcon className="tech-stack__icon" name="postgresql" />
-      <StackIcon className="tech-stack__icon" name="git" />
-      <StackIcon className="tech-stack__icon" name="html5" />
-      <StackIcon className="tech-stack__icon" name="css3" />
-      <StackIcon className="tech-stack__icon" name="js" />
-      <div className="tech-stack__icon-wrapper">
-        <StackIcon className="tech-stack__icon" name="reactjs" />
+  const iconList = () => {
+    const iconNames = [
+      "python", "java", "postgresql", "git",
+      "html5", "css3", "js", "reactjs"
+    ];
+    const textNames = [
+      "Python", "Java", "SQL\n(PostgreSQL)", "Git",
+      "HTML", "CSS", "JavaScript", "React"
+    ];
+
+    return (
+      <div className="tech-stack">
+        {iconNames.map((name, index) => (
+          index === 7 ? (
+            <div key={index} className="tech-stack__icon-container">
+              <div className="tech-stack__icon-wrapper">
+                <TechStackIcon iconName={name}/>
+              </div>
+              <p className="tech-stack__text">{textNames[index]}</p>
+            </div>
+          ) : (
+            <div key={index} className="tech-stack__icon-container">
+              <TechStackIcon key={index} iconName={name}/>
+              <p className="tech-stack__text">{textNames[index]}</p>
+            </div>
+          )
+        ))}
       </div>
+    )
+  }
+
+  return iconList();
+}
+
+function TechStackIcon({ iconName }) {
+  TechStackIcon.propTypes = {
+    iconName: PropTypes.string.isRequired,
+  }
+
+  return (
+    <div className="tech-stack__icon">
+      <StackIcon name={iconName} />
     </div>
-  )
+  );
 }
