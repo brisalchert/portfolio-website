1da0b6fdb451a915d7a8706b891add2a423049e4
brisalchert <brisalchert@gmail.com>
2024-12-06 11:03:08 -0500

    Update project variable structure and add GitHub links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,49 +5,60 @@ import clothingStore from "../assets/mock-store.png"
 
 // eslint-disable-next-line react/display-name
 const Projects = forwardRef((props, ref) => {
-  const projectTitles = [
-    "LSTM Text Classifier",
-    "Sudoku Desktop Application",
-    "Python Maze Solver",
-    "Battlefield Audio Classifier",
-    "Mock Clothing Store"
-  ];
-  const projectDescriptions = [
-    "A long short-term memory recurrent neural network for classifying text as either AI-generated or human-written.",
-
-    "A Java desktop application for playing Sudoku. Uses a backtracking algorithm to generate randomized game boards " +
-    "with only one valid solution.",
-
-    "A python application that can randomly generate mazes of various sizes and then solve them using several " +
-    "traversal algorithms. The application includes a user interface created using PyQt6 and includes a 'slow factor' " +
-    "that slows down the generation and traversal algorithms to allow the user to watch them work.",
-
-    "A convolutional neural network for classifying sound samples as one of five different types of audio from a " +
-    "battlefield setting. This project uses Tensorflow for machine learning.",
-
-    "A Java application for running a mock clothing store from the terminal. The application uses a HashMap for " +
-    "efficiently storing tens of thousands of items with constant retrieval time by ID. It also allows users to " +
-    "search for items by their characteristics and sort results by several criteria. The program implements spelling " +
-    "correction and word suggestion for many of the available input actions."
+  const projects = [
+    {
+      projectTitle: "LSTM Text Classifier",
+      projectDescription: "A long short-term memory recurrent neural network for classifying text as either" +
+        "AI-generated or human-written.",
+      projectImage: null,
+      projectURL: "https://github.com/brisalchert/ai-text-classification"
+    },
+    {
+      projectTitle: "Sudoku Desktop Application",
+      projectDescription: "A Java desktop application for playing Sudoku. Uses a backtracking algorithm to generate " +
+        "randomized game boards with only one valid solution.",
+      projectImage: null,
+      projectURL: "https://github.com/brisalchert/SudokuDesktopApplication"
+    },
+    {
+      projectTitle: "Python Maze Solver",
+      projectDescription: "A python application that can randomly generate mazes of various sizes and then solve " +
+        "them using several traversal algorithms. The application includes a user interface created using PyQt6 " +
+        "and includes a 'slow factor' that slows down the generation and traversal algorithms to allow the user " +
+        "to watch them work.",
+      projectImage: null,
+      projectURL: "https://github.com/brisalchert/maze-solver"
+    },
+    {
+      projectTitle: "Battlefield Audio Classifier",
+      projectDescription: "A convolutional neural network for classifying sound samples as one of five different " +
+        "types of audio from a battlefield setting. This project uses Tensorflow for machine learning.",
+      projectImage: null,
+      projectURL: "https://github.com/brisalchert/BattlefieldAudioClassification"
+    },
+    {
+      projectTitle: "Mock Clothing Store",
+      projectDescription: "A Java application for running a mock clothing store from the terminal. The " +
+        "application uses a HashMap for efficiently storing tens of thousands of items with constant retrieval " +
+        "time by ID. It also allows users to search for items by their characteristics and sort results by " +
+        "several criteria. The program implements spelling correction and word suggestion for many of the " +
+        "available input actions.",
+      projectImage: clothingStore,
+      projectURL: "https://github.com/brisalchert/Data-Structures-Project"
+    }
   ]
-
-  const projectImages = [
-    null,
-    null,
-    null,
-    null,
-    clothingStore
-  ];
 
   return (
     <div className="projects" ref={ref}>
       <p className="projects-title">Here are some of my projects</p>
       <div className="projects__container">
-        {projectTitles.map((title, index) => (
-          <div className="projects__item" key={index}>
-            <img src={projectImages[index]} className="projects__image" alt={title} />
-            <ProjectCard title={title} description={projectDescriptions[index]} />
-          </div>
+        {projects.map((project, index) => (
+          <a href={project["projectURL"]} key={index} target="_blank">
+            <div className="projects__item">
+              <img src={project["projectImage"]} className="projects__image" alt={project["projectTitle"]}/>
+              <ProjectCard title={project["projectTitle"]} description={project["projectDescription"]}/>
+            </div>
+          </a>
         ))}
       </div>
     </div>
