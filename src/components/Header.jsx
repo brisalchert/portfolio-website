0f256c8b69718c88d686a7ce3c7f84ed904ee238
Brian Salchert <brisalchert@gmail.com>
2024-12-06 19:36:26 -0500

    Add container with background to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,14 @@ import './Header.css';
 // eslint-disable-next-line react/prop-types
 export default function Header({ onLanguagesClick, onProjectsClick }) {
   return (
-    <nav className="nav-header">
-      <p>Brian Salchert</p>
-      <ul>
-        <li><a onClick={onLanguagesClick}>Languages & Tools</a></li>
-        <li><a onClick={onProjectsClick}>Projects</a></li>
-      </ul>
-    </nav>
+    <div className="header">
+      <nav className="nav-header">
+        <p>Brian Salchert</p>
+        <ul>
+          <li><a onClick={onLanguagesClick}>Languages & Tools</a></li>
+          <li><a onClick={onProjectsClick}>Projects</a></li>
+        </ul>
+      </nav>
+    </div>
   );
 }
