956a101d2b7bf14e0e1696d66ebdb4baa342085b
Brian Salchert <brisalchert@gmail.com>
2024-12-07 14:45:29 -0500

    Add header links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,24 @@
 import './Header.css';
+import linkedin from "../assets/linkedin.png"
+import github from "../assets/github.png"
+import resume from "../assets/resume.png"
 
 // eslint-disable-next-line react/prop-types
 export default function Header({ onLanguagesClick, onProjectsClick }) {
   return (
     <div className="header">
-      <nav className="nav-header">
+      <nav className="header__nav">
         <p>Brian Salchert</p>
         <ul>
+          <li><a href="https://www.linkedin.com/in/brian-salchert/" target="_blank">
+            <img src={linkedin} className="header__icon" alt="LinkedIn"/>
+          </a></li>
+          <li><a href="https://github.com/brisalchert" target="_blank">
+            <img src={github} className="header__icon" alt="LinkedIn"/>
+          </a></li>
+          <li><a href="/resume.pdf" target="_blank">
+            <img src={resume} className="header__icon" alt="LinkedIn"/>
+          </a></li>
           <li><a onClick={onLanguagesClick}>Languages & Tools</a></li>
           <li><a onClick={onProjectsClick}>Projects</a></li>
         </ul>
