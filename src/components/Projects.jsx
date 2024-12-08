862c17d29e4c27b36492351570006f282af9d33a
Brian Salchert <brisalchert@gmail.com>
2024-12-08 12:13:35 -0500

    Polish project info text

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import "./Projects.css"
 import {forwardRef, useState} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import clothingStore from "../assets/mock-store.png"
 import mazeSolver from "../assets/maze-solver.png"
@@ -13,39 +14,51 @@ const Projects = forwardRef((props, ref) => {
   const projects = [
     {
       projectTitle: "LSTM Text Classifier",
-      projectDescription: "A long short-term memory recurrent neural network for classifying text as either" +
-        "AI-generated or human-written.",
+      projectTools: "Python, PyTorch, Machine Learning, NLP, Pandas, Seaborn, Matplotlib, PyCharm",
+      projectDescription: "A long short-term memory recurrent neural network for performing binary classification " +
+        "of text as either AI-generated or human-written.\n\nThe program preprocesses text using NLP techniques " +
+        "such as tokenization, stemming, and stop-word removal. The model implements L1 and L2 normalization " +
+        "and uses cross-validation to ensure a good tradeoff between bias and variance. The model achieves " +
+        "a testing ROC AUC score of over 95%.",
       projectImage: lstm,
       projectURL: "https://github.com/brisalchert/ai-text-classification"
     },
     {
       projectTitle: "Sudoku Desktop Application",
-      projectDescription: "A Java desktop application for playing Sudoku. Uses a backtracking algorithm to generate " +
-        "randomized game boards with only one valid solution.",
+      projectTools: "Java, JavaFX, MVC Design Pattern, IntelliJ",
+      projectDescription: "A Java desktop application for playing Sudoku. The program uses a backtracking algorithm " +
+        "to generate randomized game boards with only one valid solution.\n\nThe algorithm attempts to fill the " +
+        "board randomly by choosing valid candidates for random tiles and then eliminating candidates in other " +
+        "tiles that are made invalid by that choice.\n\nThe user interface is built in JavaFX and implements the " +
+        "Model-View-Controller design pattern.",
       projectImage: sudoku,
       projectURL: "https://github.com/brisalchert/SudokuDesktopApplication"
     },
     {
       projectTitle: "Python Maze Solver",
+      projectTools: "Python, PyQt6, PyCharm",
       projectDescription: "A python application that can randomly generate mazes of various sizes and then solve " +
-        "them using several traversal algorithms. The application includes a user interface created using PyQt6 " +
-        "and includes a 'slow factor' that slows down the generation and traversal algorithms to allow the user " +
+        "them using several traversal algorithms.\n\nThe application includes a user interface created using PyQt6 " +
+        "with a slider for a 'slow factor' that slows down the generation and traversal algorithms to allow the user " +
         "to watch them work.",
       projectImage: mazeSolver,
       projectURL: "https://github.com/brisalchert/maze-solver"
     },
     {
       projectTitle: "Battlefield Audio Classifier",
+      projectTools: "Python, Tensorflow/Keras, Machine Learning, Matplotlib, PyCharm",
       projectDescription: "A convolutional neural network for classifying sound samples as one of five different " +
-        "types of audio from a battlefield setting. This project uses Tensorflow for machine learning.",
+        "types of audio from a battlefield setting. This project uses Tensorflow for machine learning and " +
+        "achieved a validation accuracy of over 90% even with a limited dataset.",
       projectImage: audioCNN,
       projectURL: "https://github.com/brisalchert/BattlefieldAudioClassification"
     },
     {
       projectTitle: "Mock Clothing Store",
+      projectTools: "Java, Data Structures & Algorithms, IntelliJ",
       projectDescription: "A Java application for running a mock clothing store from the terminal. The " +
         "application uses a HashMap for efficiently storing tens of thousands of items with constant retrieval " +
-        "time by ID. It also allows users to search for items by their characteristics and sort results by " +
+        "time by ID.\n\nIt also allows users to search for items by their characteristics and sort results by " +
         "several criteria. The program implements spelling correction and word suggestion for many of the " +
         "available input actions.",
       projectImage: clothingStore,
@@ -119,7 +132,7 @@ function CarouselTrack({ projects, left }) {
           <a href={project["projectURL"]} target="_blank">
             <div className="projects__item">
               <img src={project["projectImage"]} className="projects__image" alt={project["projectTitle"]}/>
-              <ProjectCard title={project["projectTitle"]} description={project["projectDescription"]}/>
+              <ProjectCard title={project["projectTitle"]} tools={project["projectTools"]} description={project["projectDescription"]}/>
             </div>
           </a>
         </div>
@@ -128,9 +141,10 @@ function CarouselTrack({ projects, left }) {
   )
 }
 
-function ProjectCard({title, description}) {
+function ProjectCard({title, tools, description}) {
   ProjectCard.propTypes = {
     title: PropTypes.string,
+    tools: PropTypes.string,
     description: PropTypes.string
   }
 
@@ -138,7 +152,15 @@ function ProjectCard({title, description}) {
     <div className="projects__card">
       <div className="projects__info">
         <p className="projects__name">{title}</p>
-        <p className="projects__description">{description}</p>
+        <p className="projects__tools">{tools}</p>
+        <p className="projects__description">
+          {description.split("\n").map((line, index) => (
+              <React.Fragment key={index}>
+                {line}
+                <br />
+              </React.Fragment>
+            ))}
+        </p>
       </div>
     </div>
   )
