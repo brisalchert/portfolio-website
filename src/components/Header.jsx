b29ea9389574bd1e3986c955c76ffac86c5a4ee8
Brian Salchert <brisalchert@gmail.com>
2024-12-08 11:05:11 -0500

    Fix alt text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,10 +14,10 @@ export default function Header({ onLanguagesClick, onProjectsClick }) {
             <img src={linkedin} className="header__icon" alt="LinkedIn"/>
           </a></li>
           <li><a href="https://github.com/brisalchert" target="_blank">
-            <img src={github} className="header__icon" alt="LinkedIn"/>
+            <img src={github} className="header__icon" alt="GitHub"/>
           </a></li>
           <li><a href="/resume.pdf" target="_blank">
-            <img src={resume} className="header__icon" alt="LinkedIn"/>
+            <img src={resume} className="header__icon" alt="Resume"/>
           </a></li>
           <li><a onClick={onLanguagesClick}>Languages & Tools</a></li>
           <li><a onClick={onProjectsClick}>Projects</a></li>
